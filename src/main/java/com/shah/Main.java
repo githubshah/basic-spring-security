852faccec334860e92a7bd78c2f9a136b1a70838
shaid-hussain <shaid@gappsexperts.com>
2020-12-27 09:02:25 +0530

    refector logs

---
+++ src/main/java/com/shah/Main.java
@@ -0,0 +1,14 @@
+package com.shah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// https://www.youtube.com/watch?v=X80nJ5T7YpE
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/main/java/com/shah/springsecurity/jwt/controller/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.shah.springsecurity.jwt.controller;
+
+import com.shah.springsecurity.jwt.model.AuthenticationRequest;
+import com.shah.springsecurity.jwt.model.AuthenticationResponse;
+import com.shah.springsecurity.jwt.service.ExtUserDetailService;
+import com.shah.springsecurity.jwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private ExtUserDetailService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authentication(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        // standard token which spring takes from UI
+        try {
+            authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException badCredentialsException) {
+            throw new Exception("Bad credential");
+        }
+
+        UserDetails userDetails =
+            userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+    }
+
+}
