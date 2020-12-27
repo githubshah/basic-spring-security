a59a90c1e84b0ec3ebb47fe1b20cc2a37fea3ae3
shaid-hussain <shaid@gappsexperts.com>
2020-12-27 09:08:57 +0530

    refector logs

--- src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
+++ src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
@@ -1,6 +1,5 @@
 package com.shah.springsecurity.jwt.config;
 
-import com.shah.springsecurity.jwt.filter.JwtRequestFilter;
 import com.shah.springsecurity.jwt.service.ExtUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,27 +8,23 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private ExtUserDetailService extUserDetailService;
 
     @Autowired
-    private JwtRequestFilter jwtRequestFilter;
+    private ExtUserDetailService userDetailService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(extUserDetailService);
+        auth.userDetailsService(userDetailService);
     }
 
     // treat incoming password without any hashing techniques.
-    // else throw exception There is no PasswordEncoder mapped for the id "null"
+// else throw exception There is no PasswordEncoder mapped for the id "null"
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
@@ -39,14 +34,11 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests().antMatchers("/authenticate").permitAll()
-            .anyRequest().authenticated()
-            .and().sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+            .anyRequest().authenticated();
     }
 
-    // AuthenticationManager Bean does not provided by the new Spring version 2.4.1.
+    // Need to add in new spring framework version
+    // AuthenticationManager in your configuration
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
