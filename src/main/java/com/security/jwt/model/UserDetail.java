1990316146570a1406bf0d2d9ca1c05d30d87dc5
shaid-hussain <shaid@gappsexperts.com>
2020-12-27 07:48:18 +0530

    add depandencies and JwtUtils

---
+++ src/main/java/com/security/jwt/model/UserDetail.java
@@ -0,0 +1,22 @@
+package com.security.jwt.model;
+
+public class UserDetail {
+    String userName;
+    String userPassword;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
