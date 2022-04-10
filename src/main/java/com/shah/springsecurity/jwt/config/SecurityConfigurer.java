8338341ab75679001583223a347abc8c8a47bac2
githubshah <72010111+githubshah@users.noreply.github.com>
2022-04-10 15:57:01 +0530

    auth provider instead of User detail service

--- src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
+++ src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
@@ -1,11 +1,10 @@
 package com.shah.springsecurity.jwt.config;
 
+import com.shah.springsecurity.jwt.config.provider.UserAuthenticationProviderImpl;
 import com.shah.springsecurity.jwt.service.ExtUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,9 +15,17 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private ExtUserDetailService userDetailService;
 
+    @Autowired
+    UserAuthenticationProviderImpl provider;
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailService);
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService);
+        auth.authenticationProvider(provider);
     }
 
     // treat incoming password without any hashing techniques.
---
+++ src/main/java/com/shah/springsecurity/jwt/config/provider/UserAuthenticationProviderImpl.java
@@ -0,0 +1,51 @@
+package com.shah.springsecurity.jwt.config.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component("customUserNamePasswordSecurityProvider")
+public class UserAuthenticationProviderImpl implements AuthenticationProvider {
+
+    Map<String, User> userMap = new HashMap<>();
+
+    public UserAuthenticationProviderImpl() {
+        userMap.put("u1", new User("u1", "p1", Arrays.asList(new SimpleGrantedAuthority("read"))));
+        userMap.put("u2", new User("u2", "p2", Arrays.asList(new SimpleGrantedAuthority("read"))));
+        userMap.put("u3", new User("u3", "p3", Arrays.asList(new SimpleGrantedAuthority("read"))));
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        User user = userMap.get(name);
+        if (user == null || !user.getPassword().endsWith(pwd)) {
+            throw new BadCredentialsException("Invalid user");
+        }
+        final List<GrantedAuthority> grantedAuths = new ArrayList<>();
+        grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+        final UserDetails principal = new User(name, pwd, grantedAuths);
+        final Authentication auth = new UsernamePasswordAuthenticationToken(principal, "p1", grantedAuths);
+        return auth;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
+
