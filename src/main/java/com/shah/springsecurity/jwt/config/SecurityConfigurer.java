7b96c5a01e9c279d03f3cd7b3f60c40652a33637
githubshah <72010111+githubshah@users.noreply.github.com>
2022-04-10 16:37:16 +0530

    done

--- src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
+++ src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
@@ -3,13 +3,19 @@ package com.shah.springsecurity.jwt.config;
 import com.shah.springsecurity.jwt.service.ExtUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -19,6 +25,24 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest httpServletRequest) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:63342"));
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        return config;
+                    }
+                })
+                .and().authorizeRequests().anyRequest().permitAll()
+                .and().httpBasic()
+                .and().formLogin();
     }
 
     // treat incoming password without any hashing techniques.
