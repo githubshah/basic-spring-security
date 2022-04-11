710156e81cc36056fa91748b2eb76ba9a740a10b
githubshah <72010111+githubshah@users.noreply.github.com>
2022-04-12 02:27:51 +0530

    cloack

--- src/main/java/com/shah/rest/HelloController.java
+++ src/main/java/com/shah/rest/HelloController.java
@@ -1,22 +1,23 @@
 package com.shah.rest;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String home(OAuth2AuthenticationToken object) {
-        return "<h1>Welcome Home Secured</h1>";
+    public String Yee() {
+        return "<h1>Welcome pro</h1>";
     }
 
-    @GetMapping("/redirect")
-    public String redirect(OAuth2AuthenticationToken object) {
-        System.out.println(object);
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Welcome User</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
         return "<h1>Welcome Admin</h1>";
     }
-
 }
--- src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
+++ src/main/java/com/shah/springsecurity/jwt/config/SecurityConfigurer.java
@@ -4,39 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http.authorizeRequests()
+                .antMatchers("/user").hasAnyRole("USER")
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .antMatchers("/").authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(
-                clientRegistrationRepository());
-    }
 
-    @Bean
-    ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration google = CommonOAuth2Provider.GOOGLE.getBuilder("google")
-                .clientId("171111768645-p9nkt4fgmb4qth918e4tpnh9pfvdq5vu.apps.googleusercontent.com")
-                .clientSecret("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")
-                .build();
-        return new InMemoryClientRegistrationRepository(google);
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
